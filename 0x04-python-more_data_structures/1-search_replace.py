a27dc9eb322fb093598479b712d6ffb99b159ad0
mukwayaedreen <edmukwaya@gmail.com>
2022-09-29 05:58:03 -0700

    my dictionary and set implementation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
+# 1-search_replace.py
+
+
+def search_replace(my_list, search, replace):
+    """
+    Replace all occurrences of an element by another in a new list.
+    """
+    update_list = my_list[:]
+    for elem in range(len(update_list)):
+        if update_list[elem] == search:
+            update_list[elem] = replace
+    return (update_list)
